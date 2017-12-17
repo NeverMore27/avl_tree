ec890c193e4a24b48cb3a9899acf1614c0f2210e
NeverMore27 <uragan.uragan.59@mail.ru>
2017-12-17 14:46:43 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -8,3 +8,21 @@ SCENARIO("null")
 	avl_tree<int, int> a;
 	REQUIRE(a.isEmpty());
 }
+SCENARIO("add")
+{
+	RBtree <int, int> a;
+	a.insert(5, 6);
+	REQUIRE(!a.isEmpty());
+}
+SCENARIO("BST delete non inserted element", "[delete]")
+{
+	RBtree<int, int> tree = { {8, 7} };
+	tree.Remove(4);
+	REQUIRE(!tree.isEmpty());
+}
+SCENARIO("BST delete root with one element", "[delete]")
+{
+	RBtree<int, int> tree = { {8,8} };
+	tree.Remove(8);
+	REQUIRE(tree.isEmpty());
+}
